43859c82177663bbde7efec723afc849b8ef4234
张健顺 <1224522500@qq.com>
2017-12-10 16:35:13 +0800

    使用程序来配置注入redis

--- ssm_demo/src/main/java/me/elvis/demo/cache/redis/RedisConfig.java
+++ ssm_demo/src/main/java/me/elvis/demo/cache/redis/RedisConfig.java
@@ -1,10 +1,72 @@
 package me.elvis.demo.cache.redis;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.lang.reflect.Method;
 
 /**
  * Version:v1.0 (description:  ) Date:2017/12/8 0008  Time:17:21
  */
+@Configuration
+@EnableCaching
 public class RedisConfig extends CachingConfigurerSupport {
 
+	@Bean
+	public JedisConnectionFactory getRedisConnectionFactory() {
+		JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+
+		jedisConnectionFactory.setHostName("127.0.0.1");
+		jedisConnectionFactory.setPort(6379);
+		return jedisConnectionFactory;
+	}
+
+	/**
+	 * 为template绑定factory
+	 * @param factory
+	 * @return
+	 */
+	@Bean
+	public RedisTemplate<String,String> getRedisTemplate(RedisConnectionFactory factory) {
+		RedisTemplate<String,String> template = new RedisTemplate<>();
+		template.setConnectionFactory(factory);
+		return template;
+	}
+
+	@Bean
+	public CacheManager getCacheManager(RedisTemplate template) {
+		RedisCacheManager manager = new RedisCacheManager(template);
+		manager.setDefaultExpiration(3000);
+		return manager;
+	}
+
+	/**
+	 * 自定义Redis主键生成策略
+	 * @return
+	 */
+	@Bean
+	@Override
+	public KeyGenerator keyGenerator() {
+		return new KeyGenerator() {
+
+			@Override
+			public Object generate(Object o, Method method, Object... objects) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(o.getClass().getName());
+				sb.append(method.getName());
+				for (Object obj : objects) {
+					sb.append(obj.toString());
+				}
+				return sb.toString();
+			}
+		};
+	}
 }
--- ssm_demo/src/main/java/me/elvis/demo/support/mapper/CityMapper.java
+++ ssm_demo/src/main/java/me/elvis/demo/support/mapper/CityMapper.java
@@ -1,6 +1,9 @@
 package me.elvis.demo.support.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 import me.elvis.demo.support.pojo.City;
 
@@ -8,4 +11,20 @@ import me.elvis.demo.support.pojo.City;
 public interface CityMapper {
 
 	boolean insertCity(City city);
+
+	List<City> searchCities(
+			@Param("cityId")
+					Integer cityId,
+			@Param("cityCode")
+					String cityCode,
+			@Param("cityLevel")
+					String cityLevel,
+			@Param("provinceCode")
+					String provinceCode,
+			@Param("cityName")
+					String cityName,
+			@Param("city")
+					String city,
+			@Param("province")
+					String province);
 }
