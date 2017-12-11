37071d35e25cdaf62a5b95830286dfd24e324a27
张健顺 <1224522500@qq.com>
2017-12-11 15:55:19 +0800

    提交对应的配置文件

---
+++ ssm_demo/src/main/java/me/elvis/demo/interceptor/MethodCacheInterceptor.java
@@ -0,0 +1,8 @@
+package me.elvis.demo.interceptor;
+
+/**
+ * Version:v1.0 (description:  ) Date:2017/12/11 0011  Time:15:04
+ */
+public class MethodCacheInterceptor {
+
+}
---
+++ ssm_demo/src/main/java/me/elvis/demo/support/util/RedisUtil.java
@@ -0,0 +1,8 @@
+package me.elvis.demo.support.util;
+
+/**
+ * Version:v1.0 (description:  ) Date:2017/12/11 0011  Time:15:06
+ */
+public class RedisUtil {
+
+}
